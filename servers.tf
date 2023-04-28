c881534ed29305813833655881b8f3dddd514db8
srikaanth <srikaanth62@gmail.com>
2023-04-28 10:45:17 +0530

    cart common

--- servers.tf
+++ servers.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = "frontend"
@@ -19,6 +20,10 @@ data "aws_ami" "centos" {
   owners      = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
+}
+
+data "aws_security_group" "allow-all" {
+  Name = "allow-all"
 }
 
 resource "aws_instance" "mongodb" {
