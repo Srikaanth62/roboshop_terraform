18069807442869c1a5106f4f757007fefe13d665
srikaanth <srikaanth62@gmail.com>
2023-05-06 09:58:19 +0530

    terraform example

--- servers.tf
+++ servers.tf
@@ -1,203 +1,182 @@
-resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
+data "aws_ami" "centos" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+resource "aws_instance" "frontend" {
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "frontend"
   }
 }
 
-variable "instance_type" {
-  default = "t3.small"
-}
-
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "frontend-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
-}
-
-data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-data "aws_security_group" "allow-all" {
-  Name = "allow-all"
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "mongodb"
   }
 }
 
 resource "aws_route53_record" "mongodb" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "mongodb-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.mongodb.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "catalogue"
   }
 }
 
 resource "aws_route53_record" "catalogue" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "catalogue-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.catalogue.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
-
-
 resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "redis"
   }
 }
 
 resource "aws_route53_record" "redis" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "redis-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.redis.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "user" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "user"
   }
 }
 
 resource "aws_route53_record" "user" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "user-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.user.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "cart" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "cart"
   }
 }
 
 resource "aws_route53_record" "cart" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "cart-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.cart.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "mysql"
   }
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "mysql-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.mysql.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "shipping" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "shipping"
   }
 }
 
 resource "aws_route53_record" "shipping" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "shipping-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.shipping.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "rabbitmq"
   }
 }
 
 resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "rabbitmq-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.rabbitmq.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "payment" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "payment"
   }
 }
 
 resource "aws_route53_record" "payment" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "payment-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.payment.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
 
 resource "aws_instance" "dispatch" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-
+  ami = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
   tags = {
     Name = "dispatch"
   }
 }
 
 resource "aws_route53_record" "dispatch" {
-  zone_id = "Z088180210HCZBPL2XI2M"
   name    = "dispatch-dev.srikaanth62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.dispatch.private_ip]
+  zone_id = "Z088180210HCZBPL2XI2M"
 }
