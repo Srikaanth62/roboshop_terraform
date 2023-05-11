c062f51fbbf74f20e4c0054e09bff98448456e82
srikaanth <srikaanth62@gmail.com>
2023-05-11 11:11:15 +0530

    ansible vars

--- servers.tf
+++ servers.tf
@@ -1,58 +1,23 @@
-variable "components" {
-  default = {
-    frontend = {
-      name = "frontend"
-      instance_type = "t3.micro"
-    }
-    mongodb = {
-      name = "mongodb"
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      name = "catalogue"
-      instance_type = "t3.micro"
-    }
-    redis = {
-      name = "redis"
-      instance_type = "t3.micro"
-    }
-    user = {
-      name = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name = "cart"
-      instance_type = "t3.micro"
-    }
-    mysql = {
-      name = "mysql"
-      instance_type = "t3.micro"
-    }
-    shipping = {
-      name = "shipping"
-      instance_type = "t3.micro"
-    }
-    rabbitmq = {
-      name = "rabbitmq"
-      instance_type = "t3.micro"
-    }
-    payment = {
-      name = "payment"
-      instance_type = "t3.micro"
-    }
-    dispatch = {
-      name = "dispatch"
-      instance_type = "t3.micro"
-    }
-  }
-}
+
 
 variable "env" {
   default = [ "dev", "prod"]
 }
 
-module "servers" {
-  for_each = var.components
+module "db_servers" {
+  for_each = var.db_servers
+  source = "./module"
+
+  components_name = each.value["name"]
+  envi             = var.env
+  instance_type   = each.value["instance_type"]
+  password        = lookup(each.value, "password", "null" )
+  provisioner = true
+}
+
+module "app_servers" {
+  depends_on = [ module.db_servers ]
+  for_each = var.app_servers
   source = "./module"
 
   components_name = each.value["name"]
