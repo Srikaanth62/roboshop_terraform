a1d9cc9cdb5bec88221ea6d35fb3d39c14df2442
srikaanth <srikaanth62@gmail.com>
2023-05-13 09:21:14 +0530

    terraform vars

--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
+  instance_type = var.components_name
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
   tags = {
     Name = local.name
