d1a2f042c44b85c813148d64c51b545f9265f4d9
srikaanth <srikaanth62@gmail.com>
2023-05-13 10:00:38 +0530

    terraform vars

--- vars.tf
+++ vars.tf
@@ -1,40 +1,42 @@
 variable "env" {}
 variable "app_servers" {
-frontend = {
-name          = "frontend"
-instance_type = "t3.small"
-}
+ default = {
+   frontend = {
+     name          = "frontend"
+     instance_type = "t3.small"
+   }
 
-catalogue = {
-name          = "catalogue"
-instance_type = "t3.micro"
-}
+   catalogue = {
+     name          = "catalogue"
+     instance_type = "t3.micro"
+   }
 
-user = {
-name          = "user"
-instance_type = "t3.micro"
-}
-cart = {
-name          = "cart"
-instance_type = "t3.micro"
-}
+   user = {
+     name          = "user"
+     instance_type = "t3.micro"
+   }
+   cart = {
+     name          = "cart"
+     instance_type = "t3.micro"
+   }
 
-shipping = {
-name          = "shipping"
-instance_type = "t3.medium"
-password      = "RoboShop@1"
-}
+   shipping = {
+     name          = "shipping"
+     instance_type = "t3.medium"
+     password      = "RoboShop@1"
+   }
 
-payment = {
-name          = "payment"
-instance_type = "t3.small"
-password      = "roboshop123"
-}
+   payment = {
+     name          = "payment"
+     instance_type = "t3.small"
+     password      = "roboshop123"
+   }
 
-dispatch = {
-name          = "dispatch"
-instance_type = "t3.micro"
-}
+   dispatch = {
+     name          = "dispatch"
+     instance_type = "t3.micro"
+   }
+ }
 }
 variable "db_servers" {
   rabbitmq = {
