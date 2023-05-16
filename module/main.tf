8714a4472658793860fad9322e07f3af6f3f2df4
srikaanth <srikaanth62@gmail.com>
2023-05-16 10:20:19 +0530

    ansible loop

--- module/main.tf
+++ module/main.tf
@@ -54,4 +54,33 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_role_policy" "ssm_ps_policy" {
+  name = "${var.components_name}-${var.env}-role"
+  role = aws_iam_role.role.id
+
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:054432098243:parameter/${var.env}.${var.components_name}.*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
 
