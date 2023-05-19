5fba3ffa47f08cb36e42dd6e4c36d9a0a6ba8435
srikaanth <srikaanth62@gmail.com>
2023-05-19 23:34:07 +0530

    ansible filebeat

--- module/local.tf
+++ module/local.tf
@@ -10,4 +10,13 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/Srikaanth62/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.components_name}"
   ]
+  db_tags = {
+    Name = "${var.components_name}-${var.env}"
+  }
+  app_tags = {
+    Name      = "${var.components_name}-${var.env}"
+    Monitor   = "true"
+    component = var.components_name
+    env       = var.env
+  }
 }
