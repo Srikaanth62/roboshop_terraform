9ed27113ff4488bcd0ca94843667e932a5af8b83
srikaanth <srikaanth62@gmail.com>
2023-06-07 12:50:29 +0530

    terraform module

--- main.tf
+++ main.tf
@@ -7,10 +7,21 @@ module "vpc" {
   env = var.env
 }
 
-module "web" {
+module "app" {
   source = "git::https://github.com/Srikaanth62/tf-module-app.git"
   for_each = var.app
   instance_type = each.value["instance_type"]
-  subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0 )
+  name = each.value["name"]
+  env = var.env
+  desired_capacity = each.value["desired_capacity"]
+  min_size = each.value["min_size"]
+  max_size = each.value["max_size"]
+  bastion_cidr = var.bastion_cidr
+  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+
 }
+
+
 
