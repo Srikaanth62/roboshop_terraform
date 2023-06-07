4d0091707e76bfdfd898fe133ea84f83e76efbd2
srikaanth <srikaanth62@gmail.com>
2023-06-07 08:03:07 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::git@github.com:Srikaanth62/tf-module-vpc.git"
+  source = "https://github.com/Srikaanth62/tf-module-vpc.git"
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
   subnets = each.value["subnets"]
@@ -8,7 +8,7 @@ module "vpc" {
 }
 
 module "web" {
-  source = "git::git@github.com:Srikaanth62/tf-module-app.git"
+  source = "https://github.com/Srikaanth62/tf-module-app.git"
   for_each = var.app
   instance_type = each.value["instance_type"]
   subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0 )
