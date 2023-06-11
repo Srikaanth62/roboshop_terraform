4a343e9fb3e91daa129eaca0f78cc9a548fbd50f
srikaanth <srikaanth62@gmail.com>
2023-06-11 13:29:32 +0530

    docdb

--- vars.tf
+++ vars.tf
@@ -6,11 +6,13 @@ variable "default_vpc_cidr" {}
 variable "default_vpc_id" {}
 variable "default_vpc_rtid" {}
 variable "docdb" {
-      subnet_name    = "db"
-      allow_db_cidr  = "app"
-      engine_version = "4.0.0"
-      instance_count = 1
-      instance_class = "db.t3.medium"
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    engine_version = "4.0.0"
+    instance_count = 1
+    instance_class = "db.t3.medium"
+  }
 }
 variable "kms_arn" {}
 
